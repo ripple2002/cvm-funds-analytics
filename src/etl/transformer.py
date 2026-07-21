7b20e6391dfd39e4abeced65f4da1efd67e36e25
bernardo <bernardonunespaixao@gmail.com>
2026-07-20 22:24:44 -0300

    feat: add transformer

---
+++ src/etl/transformer.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+def transform_cad_fi(file_path):
+
+    df = pd.read_csv(
+        file_path, 
+        sep=";", 
+        encoding="iso-8859-1", 
+        dtype=str
+    )
+
+    df.columns = df.columns.str.lower().str.strip()
+
+    if "cnpj_fundo" in df.columns:
+        df["cnpj_fundo"] = (
+            df["cnpj_fundo"]
+            .str.replace(".", "", regex=False)
+            .str.replace("/", "", regex=False)
+            .str.replace("-", "", regex=False)
+        )
+        
+    for col in df.columns:
+        df[col] = df[col].str.strip()
+
+    return df
+
+
+
+
+if __name__ == "__main__":
+    path = "data/raw/cad_fi.csv"
+    clean_df = transform_cad_fi(path)
+    clean_df.to_csv("data/processed/cad_fi_limpo.csv", index=False, sep=";")
+    
