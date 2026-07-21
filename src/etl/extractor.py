c895947c7d3c85df424afc4dd0db124a42aa4f44
bernardo <bernardonunespaixao@gmail.com>
2026-07-20 22:31:35 -0300

    feat: add logic to download and treat inf_diario

--- src/etl/extractor.py
+++ src/etl/extractor.py
@@ -1,15 +1,19 @@
 from pathlib import Path
 import requests
+import zipfile
 
-
-def download_cvm_file(url: str, dest_path: str, chunk_size: int = 8192) -> Path:
+def download_cvm_file(url: str, dest_dir: str = "data/raw", chunk_size: int = 8192) -> Path:
     
-    target = Path(dest_path)
+    file_name = url.split("/")[-1]
+    
+    target = Path(dest_dir) / file_name
     
     target.parent.mkdir(parents=True, exist_ok=True)
 
-    if target.exists() and target.stat().st_size > 0:
-        return target
+    final_target = target.with_suffix(".csv") if target.suffix == ".zip" else target
+
+    if final_target.exists() and final_target.stat().st_size > 0:
+        return final_target
 
     tmp_path = target.with_suffix(target.suffix + ".tmp")
 
@@ -20,17 +24,25 @@ def download_cvm_file(url: str, dest_path: str, chunk_size: int = 8192) -> Path:
             with open(tmp_path, "wb") as tmp_file:
                 for chunk in response.iter_content(chunk_size=chunk_size):
                     if chunk:
-                        tmp_file.write(chunk)  
-      
+                        tmp_file.write(chunk)
+        
+
         tmp_path.rename(target)
 
+        if target.suffix == ".zip":
+            with zipfile.ZipFile(target, "r") as zip_ref:
+                zip_ref.extractall(target.parent)
+            
+            target.unlink() # Apaga o .zip pesado após extrair
+            return final_target
+
     except Exception:
-       
         if tmp_path.exists():
             tmp_path.unlink()
-        raise 
+        raise
 
     return target
+
 
 
 if __name__ == "__main__":
@@ -38,9 +50,11 @@ if __name__ == "__main__":
     CAMINHO_DESTINO = Path("data/raw/cad_fi.csv")
 
     print("--- Teste 1: Primeiro Download (Deve ir no servidor) ---")
-    arquivo_salvo = download_cvm_file(URL_CADASTRO, CAMINHO_DESTINO)
+    arquivo_salvo = download_cvm_file(URL_CADASTRO)
     print(f"Resultado: {arquivo_salvo}\n")
 
     print("--- Teste 2: Segundo Download (Deve bater no Cache) ---")
-    arquivo_salvo = download_cvm_file(URL_CADASTRO, CAMINHO_DESTINO)
+    arquivo_salvo = download_cvm_file(URL_CADASTRO)
     print(f"Resultado: {arquivo_salvo}")
+
+    arquivo_salvo = download_cvm_file("https://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_202606.zip")
